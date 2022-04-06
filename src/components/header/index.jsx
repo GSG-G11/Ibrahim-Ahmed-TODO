1b4df0639c29b0fe922c2d50b8731cb9c010d662
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-04-07 01:53:31 +0300

    Fix Bug in Times and Images

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import "./header.css";
-import img1 from "../../images/backGround-header.jpg";
-import img2 from "../../images/IMG_20210412_041914.jpg";
-import img3 from "../../images/Tumblr_l_45021871112375.jpg";
-import img4 from "../../images/IMG_20210417_110829.jpg";
-import img5 from "../../images/Tumblr_l_173327231835022.jpg";
+import React, { Component } from 'react';
+import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import img1 from '../../images/backGround-header.jpg';
+import img2 from '../../images/IMG_20210412_041914.jpg';
+import img3 from '../../images/Tumblr_l_45021871112375.jpg';
+import img4 from '../../images/IMG_20210417_110829.jpg';
+import img5 from '../../images/Tumblr_l_173327231835022.jpg';
 export default class index extends Component {
   state = {
     time: new Date(),
+    random: Math.floor(Math.random() * 5),
   };
 
   currentTime = () => {
@@ -16,20 +19,20 @@ export default class index extends Component {
 
   formatDate = (date) => {
     const options = {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
     };
-    return new Date(date).toLocaleDateString("en-us", options);
+    return new Date(date).toLocaleDateString('en-us', options);
   };
 
-  // componentWillMount() {
-  //   setInterval(() => this.currentTime(), 1000);
-  // }
+  componentDidMount() {
+    setInterval(() => this.currentTime(), 1000);
+  }
 
   changeImg = () => {
     const imgsName = [img1, img2, img3, img4, img5];
-    const randomImg = Math.floor(Math.random() * 5);
+    const randomImg = this.state.random;
 
     return imgsName[randomImg];
   };
@@ -38,35 +41,35 @@ export default class index extends Component {
     const { numNotes, openModalHandler } = this.props;
     const { time } = this.state;
     const d = new Date();
-    let day = d.toLocaleDateString("en-us", { weekday: "long" });
+    let day = d.toLocaleDateString('en-us', { weekday: 'long' });
 
     return (
       <div
-        className="header "
+        className='header'
         style={{
           backgroundImage: `url(${this.changeImg()})`,
-        }}
-      >
-        <div className="header-header">
-          <h1>To Do App</h1> <p> {time.toLocaleTimeString()}</p>
+        }}>
+        <div className='overlay__header'></div>
+        <div className='header-header'>
+          <h1>To Do App</h1>
+          <p>{time.toLocaleTimeString()}</p>
         </div>
-        <div className="header-bottom">
-          <div className="date-day">
-            <h3 className="day">{day}</h3>
-            <p className="date">{this.formatDate(d)}</p>
+        <div className='header-bottom'>
+          <div className='date-day'>
+            <h3 className='day'>{day}</h3>
+            <p className='date'>{this.formatDate(d)}</p>
           </div>
 
           {numNotes !== 0 && (
-            <div className="numTask">
+            <div className='numTask'>
               <p> {numNotes} tasks</p>
             </div>
           )}
         </div>
         <button
-          className="addTask"
-          onClick={() => openModalHandler("CreateNotes")}
-        >
-          +
+          className='addTask'
+          onClick={() => openModalHandler('CreateNotes')}>
+          <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
     );
