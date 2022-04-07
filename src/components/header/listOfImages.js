08caec5b75d0984639cfa3d8543eb1840a831413
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-04-07 11:12:31 +0300

    update imgs

--- src/components/header/listOfImages.js
+++ src/components/header/listOfImages.js
@@ -1,8 +1,38 @@
-import img1 from '../../images/backGround-header.jpg';
-import img2 from '../../images/IMG_20210412_041914.jpg';
-import img3 from '../../images/Tumblr_l_45021871112375.jpg';
-import img4 from '../../images/IMG_20210417_110829.jpg';
-import img5 from '../../images/Tumblr_l_173327231835022.jpg';
+import bg_1 from '../../images/bg-1.jpg';
+import bg_2 from '../../images/bg-2.jpg';
+import bg_3 from '../../images/bg-3.jpg';
+import bg_4 from '../../images/bg-4.jpg';
+import bg_5 from '../../images/bg-5.jpg';
+import bg_6 from '../../images/bg-6.jpg';
+import bg_7 from '../../images/bg-7.jpg';
+import bg_8 from '../../images/bg-8.jpg';
+import bg_9 from '../../images/bg-9.jpg';
+import bg_10 from '../../images/bg-10.jpg';
+import bg_11 from '../../images/bg-11.jpg';
+import bg_12 from '../../images/bg-12.jpg';
+import bg_13 from '../../images/bg-13.jpg';
+import bg_14 from '../../images/bg-14.jpg';
+import bg_15 from '../../images/bg-15.jpg';
+import bg_16 from '../../images/bg-16.jpg';
+import bg_17 from '../../images/bg-17.jpg';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
-export default [img1, img2, img3, img4, img5];
+export default [
+  bg_1,
+  bg_2,
+  bg_3,
+  bg_4,
+  bg_5,
+  bg_6,
+  bg_7,
+  bg_8,
+  bg_9,
+  bg_10,
+  bg_11,
+  bg_12,
+  bg_13,
+  bg_14,
+  bg_15,
+  bg_16,
+  bg_17,
+];
